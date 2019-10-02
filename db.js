99568151ab1b4d895f9aafa02fdcf1e33ee51b37
Arthur <arthurcs426@gmail.com>
2019-10-02 13:08:00 -0700

    getting the things to work in the syncAndSeed promise

--- db.js
+++ db.js
@@ -15,12 +15,23 @@ const Page = conn.define('page', {
   }
 });
 
+Page.findHomePage = async function() {
+  return await Page.findOne({ where: {title: 'Home Page'} });
+};
+
+Page.prototype.findChildren = async function() {
+ return await Page.findAll({ where: { parentId: this.id } });
+};
+
+// Associations
+Page.hasMany(Page, { foreignKey: 'parentId' });
+
 const mapAndSave = (pages) => Promise.all(pages.map(page => Page.create(page)));
 
 const syncAndSeed = async () => {
   await conn.sync({ force: true });
 
-  const home = await Page.create({ tite: 'Home Page' });
+  const home = await Page.create({ title: 'Home Page' });
 
   let pages = [
     { title: 'About', parentId: home.id },
@@ -32,7 +43,7 @@ const syncAndSeed = async () => {
   pages = [
     { title: 'About Our Team', parentId: about.id },
     { title: 'About Our History', parentId: about.id },
-    { title: 'Phone', parentId: contactId },
+    { title: 'Phone', parentId: contact.id },
     { title: 'Fax', parentId: contact.id }
   ];
 
@@ -40,25 +51,27 @@ const syncAndSeed = async () => {
 
 };
 
-syncAndSeed();
-  // .then(async ()=> {
-  //   const home = await Page.findHomePage();
-  //   console.log(home.title); // Home page
+syncAndSeed()
+  .then(async ()=> {
+    const home = await Page.findHomePage();
+    //console.log(home);
+    console.log(home.title); // Home page
 
-  //   const homeChildren = await home.findChildren();
-  //   console.log(homeChildren.map( page => page.title));
-  //   // [ About, Contact ]
+    const homeChildren = await home.findChildren();
+    console.log(homeChildren.map( page => page.title));
+    // [ About, Contact ]
 
-  //   const fax = await Page.findOne({ where: { title: 'Fax' } });
-  //   console.log(fax.title);
-  //   // hierarch returns the page, parentPage, parent's parent.. etc.
+    const fax = await Page.findOne({ where: { title: 'Fax' } });
+    console.log(fax.title);
 
-  //    let hier = await fax.hierarchy();
-  //    console.log(hier.map( page => page.title)); // ['Fax', 'Contact', 'Home']
+    // hierarch returns the page, parentPage, parent's parent.. etc.
+    let hier = await fax.hierarchy();
+    console.log(hier.map( page => page.title));
+    // ['Fax', 'Contact', 'Home']
 
   //    const history = await Page.findOne({ where: { title: 'About Our History'} });
   //    hier = await history.hierarchy();
   //    console.log(hier.map(page => page.title));
   //    // ['About Our History', 'About', 'Home Page']
-  // })
+  })
 
