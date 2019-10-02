b3935423f105dc2ada085c0ccd0eea477392d01b
Arthur <arthurcs426@gmail.com>
2019-10-02 13:47:32 -0700

    got 2/3 routes working

--- server.js
+++ server.js
@@ -4,10 +4,20 @@ const db = require('./db');
 const { Page } = db.models;
 
 app.get('/api/pages', (req, res, next) => {
-  Page.findAll()
+  Page.findAll({
+      include: [Page]
+    })
     .then(pages => res.send(pages))
     .catch(next);
 });
+
+app.get('/api/pages/:id/children', (req, res, next) => {
+  Page.findByPk(req.params.id, { include: [ Page ]})
+    .then(page => res.send(page))
+    .catch(next);
+});
+
+
 
 const port = process.env.PORT || 3000;
 
